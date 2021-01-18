a5b3aefdc33eb16ba4aeef8903eb2af9a06f6c57
Deniz Eroglu <denizeroglu@me.com>
2021-01-18 01:02:47 +0100

    Create project

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Deniz Eroglu on 2021-01-18.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
